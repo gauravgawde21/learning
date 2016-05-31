6d01ec04ad497ecf4609a79dcee739a2716c102a
Gaurav Gawde <gaurav.gawde21@gmail.com>
2016-05-30 23:16:23 -0700

    Sixth commit

---
+++ reverse_ll.c
@@ -0,0 +1,15 @@
+/* Function to reverse the linked list */
+static void reverse(struct node** head_ref)
+{
+    struct node* prev   = NULL;
+    struct node* current = *head_ref;
+    struct node* next;
+    while (current != NULL)
+    {
+        next  = current->next;  
+        current->next = prev;   
+        prev = current;
+        current = next;
+    }
+    *head_ref = prev;
+}
